0903f02e7ed04e366b843fac8ee141e588db6d46
Pablo ZULIAN <pmzulian@gmail.com>
2021-11-04 11:17:59 -0300

    Short-circuit.js.

---
+++ Short-circuiting.js
@@ -0,0 +1,17 @@
+// OR
+/*console.log(3 || 'String');
+console.log('' || 'String');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || null || 'Hello' || 20 || null);*/
+
+/*let numGuests = 0;
+const guest1 = numGuests || 10;
+console.log(guest1);*/
+
+// AND
+console.log(0 && 'String');
+console.log('String' && null);
+console.log(true && 0);
+console.log(undefined && null);

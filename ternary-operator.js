90188a7232c26be4ec8ce0d62181b9a334a00dd8
Pablo ZULIAN <pmzulian@gmail.com>
2021-11-17 10:14:09 -0300

    Operadores dentro de bucle if-else.

--- ternary-operator.js
+++ ternary-operator.js
@@ -19,7 +19,7 @@
 // console.log(drink2);
 
 //Ternary operator within string templates
-console.log(`I like drink ${age >= 18 ? "Wine" : "Water"}`);
+// console.log(`I like drink ${age >= 18 ? "Wine" : "Water"}`);
 
 // Comparison with ternary operator
 // let modsAdded = 0;
@@ -27,3 +27,21 @@ console.log(`I like drink ${age >= 18 ? "Wine" : "Water"}`);
 // console.log("isSubSection: ", isSubSection ? 1 : 0);
 // console.log(modsAdded === (isSubSection ? 1 : 0) ? 'undef' : true);
 // console.log(modsAdded === 1 ? 'undef' : true);
+
+const a = 'String';
+const b = null;
+const c = 200;
+
+console.log('Comparación a && b');
+if(a && b){
+  console.log('Ingresa al bucle');
+} else {
+  console.log('No ingresa al bucle');
+};
+
+console.log('Comparación a && c');
+if(a && c){
+  console.log('Ingresa al bucle');
+} else {
+  console.log('No ingresa al bucle');
+}

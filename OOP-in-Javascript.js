59657956e05aa1a4ae8263691d29b9115bde23e1
Pablo ZULIAN <pmzulian@gmail.com>
2021-11-25 11:37:48 -0300

    Prototypes hasOwnProperty.

--- OOP-in-Javascript.js
+++ OOP-in-Javascript.js
@@ -22,4 +22,21 @@ Person.prototype.calcAge = function() {
 }
 // console.log(Person.prototype);
 
-globant.calcAge();
+// globant.calcAge();
+
+// Property __proto__
+// console.log(globant.__proto__);
+
+// Equivalence between prototypes
+// console.log(globant.__proto__ === Person.prototype);
+
+// isPrototypeOf()
+// console.log(Person.prototype.isPrototypeOf(globant));
+// console.log(Person.prototype.isPrototypeOf(Person));
+
+// Add properties to prototype
+Person.prototype.species = 'Homo Sapiens';
+// console.log(globant);
+// console.log(globant.species);
+console.log(globant.hasOwnProperty('firstName'));
+console.log(globant.hasOwnProperty('species'));

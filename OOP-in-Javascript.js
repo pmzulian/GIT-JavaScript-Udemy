8ea2df43833b00fbdcbc0936cbebc6950e9f098d
Pablo ZULIAN <pmzulian@gmail.com>
2021-11-29 18:01:44 -0300

    Prototype and __proto__.

--- OOP-in-Javascript.js
+++ OOP-in-Javascript.js
@@ -38,5 +38,21 @@ Person.prototype.calcAge = function() {
 Person.prototype.species = 'Homo Sapiens';
 // console.log(globant);
 // console.log(globant.species);
-console.log(globant.hasOwnProperty('firstName'));
-console.log(globant.hasOwnProperty('species'));
+// console.log(globant.hasOwnProperty('firstName'));
+// console.log(globant.hasOwnProperty('species'));
+
+// __proto__
+// console.log(globant.__proto__);
+// console.log(globant.__proto__.__proto__);
+
+// Object.prototype (top of the prototype chain)
+// console.log(globant.__proto__.__proto__.__proto__);
+
+// Prototype of a function
+console.log(Person.prototype.constructor);
+
+// Prototype of an array
+const arr = [3, 6, 8, 10];
+// console.log(arr.__proto__);
+// console.log(arr.__proto__ === Array.prototype);
+console.log(arr.__proto__.__proto__);

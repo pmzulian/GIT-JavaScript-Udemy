900390d987505ddcb68bfdc8669aa64f419b7a1e
Pablo ZULIAN <pmzulian@gmail.com>
2021-12-06 10:05:27 -0300

    Conflicto con stash.

--- Classes.js
+++ Classes.js
@@ -7,11 +7,12 @@
 
 // Class Declaration
 class PersonCl {
-  constructor(firtsName, birthYear) {
-    this.firtsName = firtsName;
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
     this.birthYear = birthYear;
   };
 
+  // Methods will be added to to prototype property
   calcAge() {
     console.log(2021 - this.birthYear);
   }
@@ -24,4 +25,14 @@ console.log(jessica);
 jessica.calcAge();
 
 console.log(jessica.__proto__ === PersonCl.prototype);
+
+//Add method to class prototype
+PersonCl.prototype.greet = function () {
+  console.log(`Hello ${this.firstName}`);
+};
+
+jessica.greet();
+
+console.log('Mensaje conflicto con stash');
+
 //Fin del archivo

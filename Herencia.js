e3554376c87ba16772a475e9da4618c20d30e648
Pablo ZULIAN <pmzulian@gmail.com>
2021-12-16 19:06:39 -0300

    Herencia con Clases.

--- Herencia.js
+++ Herencia.js
@@ -42,5 +42,71 @@ console.log(mike.__proto__.__proto__.__proto__);
 console.dir(Student.prototype.constructor);
 
 // Herencia con Clases
+// Classes with static methods
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  };
+
+  // Methods will be added to the .prototype property
+  // Also call Instance Methods
+  calcAge() {
+    console.log(2021 - this.birthYear);
+  };
+
+  get age() {
+    return 2021 - this.birthYear;
+  };
+
+  // Set a property already exists
+  set fullName(name) {
+    console.log(name);
+    if(name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name`)
+  };
+
+  get fullName() {
+    return this._fullName;
+  };
+
+  // Static methods
+  static hey() {
+    console.log('Hey there!');
+    console.log(this);
+  };
+};
+
+const someone = new PersonCl('John DOE', 1965);
+PersonCl.hey();
+
+class StudentCl extends PersonCl {
+  constructor (fullName, birthYear, course) {
+    // Siempre debe ocurrir primero
+    super(fullName, birthYear);
+    this.course = course;
+  };
+
+  introduce () {
+    console.log(`My name is ${this.fullName} and I study ${this.course}`);
+  };
+
+  // Sobrescribimos método de la clase padre
+  calcAge() {
+    console.log(`I am ${2021 - this.birthYear} years old`);
+  };
+};
+
+// const peter = new StudentCl('Peter Parker', 1990);
+const peter = new StudentCl('Peter Parker', 1990, 'Computer Science');
+peter.introduce();
+peter.calcAge();
+
+
+
+
+
+
+
 
 ////

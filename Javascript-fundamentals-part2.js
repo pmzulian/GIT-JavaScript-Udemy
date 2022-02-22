d92a208a29da984da9e417285903390f25bb57ba
Pablo ZULIAN <pmzulian@gmail.com>
2022-02-22 16:24:54 -0300

    Objects

--- Javascript-fundamentals-part2.js
+++ Javascript-fundamentals-part2.js
@@ -65,9 +65,32 @@ function fruitProcessor(apples, oranges) {
 }
 console.log(fruitProcessor(2, 3));*/
 
+///////////////////////////////////////
+// Object Methods
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  birthYeah: 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven'],
+  hasDriversLicense: true,
+  // calcAge: function (birthYeah) {
+  //   return 2037 - birthYeah;
+  // }
+  calcAge: function () {
+    console.log(this);
+    return 2037 - this.birthYeah;
+  }
+/*  calcAge: function () {
+    this.age = 2037 - this.birthYeah;
+    return this.age;
+  },
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()}-year old ${jonas.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
+  }*/
+};
 
-
-
+jonas.calcAge();
 
 
 

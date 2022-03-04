028ff87a5fc43e90afa241eb113d03887e56cb8a
Pablo ZULIAN <pmzulian@gmail.com>
2022-03-04 17:45:28 -0300

    Loop for and While.

--- Javascript-fundamentals-part2.js
+++ Javascript-fundamentals-part2.js
@@ -67,7 +67,7 @@ console.log(fruitProcessor(2, 3));*/
 
 ///////////////////////////////////////
 // Object Methods
-const jonas = {
+/*const jonas = {
   firstName: 'Jonas',
   lastName: 'Schmedtmann',
   birthYeah: 1991,
@@ -80,17 +80,98 @@ const jonas = {
   calcAge: function () {
     console.log(this);
     return 2037 - this.birthYeah;
-  }
+  }*/
 /*  calcAge: function () {
     this.age = 2037 - this.birthYeah;
     return this.age;
   },
   getSummary: function () {
     return `${this.firstName} is a ${this.calcAge()}-year old ${jonas.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
-  }*/
-};
+  }
+};*/
 
-jonas.calcAge();
+// jonas.calcAge();
+
+///////////////////////////////////////
+// Looping Arrays, Breaking and Continuing
+const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven'],
+  true
+];
+const types = [];
+// console.log(jonas[0])
+// console.log(jonas[1])
+// ...
+// console.log(jonas[4])
+// jonas[5] does NOT exist
+/*for (let i = 0; i < jonas.length; i++) {
+  // Reading from jonas array
+  console.log(jonas[i], typeof jonas[i]);
+  // Filling types array
+  // types[i] = typeof jonas[i];
+  types.push(typeof (jonas[i]));
+}
+console.log(types);
+*/
+/*const years = [1991, 2007, 1969, 2020];
+const ages = [];
+for (let i = 0; i < years.length; i++) {
+  ages.push(2037 - years[i]);
+}
+console.log(ages);*/
 
 
+// continue and break
+/*console.log('--- ONLY STRINGS ---')
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] !== 'string') continue;
+  console.log(jonas[i], typeof jonas[i]);
+}
+console.log('--- BREAK WITH NUMBER ---')
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] === 'number') break;
+  console.log(jonas[i], typeof jonas[i]);
+}*/
+
+///////////////////////////////////////
+// Looping Backwards and Loops in Loops
+/*const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven'],
+  true
+];
+// 0, 1, ..., 4
+// 4, 3, ..., 0
+for (let i = jonas.length - 1; i >= 0; i--) {
+  console.log(i, jonas[i]);
+}
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`-------- Starting exercise ${exercise}`);
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`);
+  }
+}*/
+
+// The while Loop
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
+}
+let rep = 1;
+while (rep <= 10) {
+  // console.log(`WHILE: Lifting weights repetition ${rep} 🏋️‍♀️`);
+  rep++;
+}
+let dice = Math.trunc(Math.random() * 6) + 1;
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log('Loop is about to end...');
+}
 

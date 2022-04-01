ee1f5367a9e1cdc0b13ea272ccb5f9fa0d20c5ff
Pablo ZULIAN <pmzulian@gmail.com>
2022-04-01 12:01:14 -0300

    Logica completa.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,47 @@
 'use strict';
+
+/*console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!'
+
+document.querySelector('.number').textContent = 18;
+document.querySelector('.score').textContent = 35;
+
+document.querySelector('.guess').value = 24;*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function() {
+  const guess = Number (document.querySelector('.guess').value);
+  console.log(guess);
+
+  // When there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number';
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!'
+
+    // When guess is too high
+  } else if (guess > secretNumber) {
+    if(score > 1) {
+      document.querySelector('.message').textContent = 'Too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Lost the game';
+    }
+
+    // When guess is too low
+  } else if (guess < secretNumber) {
+    if(score > 1) {
+      document.querySelector('.message').textContent = 'Too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Lost the game';
+    }
+  }
+});

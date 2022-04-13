93202fcfe838ac8ddb8c04562706c34b42e7ce0b
Pablo ZULIAN <pmzulian@gmail.com>
2022-04-13 11:52:15 -0300

    DRY y refactorización.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,17 +12,22 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+const displayMessage = function(message) {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function() {
   const guess = Number (document.querySelector('.guess').value);
   console.log(guess);
 
   // When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = 'No number';
+    // document.querySelector('.message').textContent = 'No number';
+    displayMessage('No number');
 
     // When player wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number!';
+    displayMessage('Correct Number');
     document.querySelector('body').style.backgroundColor = '#06b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = secretNumber;
@@ -33,23 +38,35 @@ document.querySelector('.check').addEventListener('click', function() {
     }
 
     // When guess is too high
-  } else if (guess > secretNumber) {
+  /*} else if (guess > secretNumber) {
     if(score > 1) {
       document.querySelector('.message').textContent = 'Too high';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = 'Lost the game';
-    }
+    }*/
 
     // When guess is too low
-  } else if (guess < secretNumber) {
+  /*} else if (guess < secretNumber) {
     if(score > 1) {
       document.querySelector('.message').textContent = 'Too low';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = 'Lost the game';
+    }
+  }*/
+
+  // Refactor for number to high or low, when guess is wrong
+} else if (guess !== secretNumber) {
+    if(score > 1) {
+      displayMessage(guess > secretNumber ? 'Too high' : 'Too low');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('Lost the game');
+      document.querySelector('.score').textContent = 0;
     }
   }
 });
@@ -72,7 +89,7 @@ document.querySelector('.again').addEventListener('click', function() {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   score = 20;
 
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.score').textContent = score;
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.guess').value = '';

edf4326864ab9e0cec9dea60bf2797a990ac1d09
Pablo ZULIAN <pmzulian@gmail.com>
2022-04-29 18:26:36 -0300

    Ventana modal.

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,7 +1,20 @@
 'use strict';
 
+// Primer paso: seleccionar todo lo que vamos a necesitar
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelector('.open-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', function() {
+      console.log('Button clicked');
+      modal.classList.remove('hidden');
+      overlay.classList.remove('hidden');
+  });
+
+btnCloseModal.addEventListener('click', () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});

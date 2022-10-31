1e54a022d108bcd8abb05f5616011a3d5385ae73
Pablo ZULIAN <pmzulian@gmail.com>
2022-10-31 12:21:16 -0300

    Callback Hell with setTimeOut

--- Asynchronous-JS/script.js
+++ Asynchronous-JS/script.js
@@ -4,7 +4,7 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-const getCountryData = function (country) {
+/* const getCountryData = function (country) {
     const request = new XMLHttpRequest();
     request.open('GET', 'https://restcountries.com/v2/name/' + country);
     request.send();
@@ -27,9 +27,78 @@ const getCountryData = function (country) {
   countriesContainer.insertAdjacentHTML('beforeend', html);
   countriesContainer.style.opacity = 1;
     });
+}; */
+
+// getCountryData('turkey');
+// getCountryData('brazil');
+// getCountryData('germany');
+
+const renderCountry = function(data, className = '') {
+    const html = `<article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+}
+
+const getCountryAndNeighbour = function (country) {
+
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', 'https://restcountries.com/v2/name/' + country);
+  request.send();
+
+  request.addEventListener('load', function() {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render conuntry 1
+    renderCountry(data);
+
+    // Get neighbour country
+    const neighbour = data.borders?.[0];
+    if (!neighbour) return;
+
+      // AJAX call country 2
+      const request2 = new XMLHttpRequest();
+      request2.open('GET', 'https://restcountries.com/v2/alpha/' + neighbour);
+      request2.send();
+
+      request2.addEventListener('load', function() {
+        const data2 = JSON.parse(this.responseText);
+        console.log(data2);
+
+        // Render conuntry 2
+        renderCountry(data2, 'neighbour');
+      });
+  });
 };
 
-getCountryData('turkey');
-getCountryData('brazil');
-getCountryData('germany');
+getCountryAndNeighbour('turkey');
 
+setTimeout(() => {
+  console.log('One second passed');
+
+  setTimeout(() => {
+    console.log('Two seconds passed');
+
+    setTimeout(() => {
+      console.log('Three seconds passed');
+
+      setTimeout(() => {
+        console.log('Four seconds passed');
+      }, 1000);
+
+    }, 1000);
+  
+  }, 1000);
+
+}, 1000);
